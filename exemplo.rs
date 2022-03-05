a911056d5ca50da298910efe04d0a14d748e4352
Gabriel Araújo <gabriel140492@gmail.com>
2022-03-05 13:08:32 -0300

    Add OpenData and Wrapper to FileOperations

--- exemplo.rs
+++ exemplo.rs
@@ -8,6 +8,7 @@ use kernel::file_operations::FileOperations;
 use kernel::io_buffer::{IoBufferReader, IoBufferWriter};
 use kernel::prelude::*;
 use kernel::miscdev;
+use kernel::sync::{Ref, RefBorrow};
 
 module! {
     type: Exemplo,
@@ -15,25 +16,36 @@ module! {
     license: b"GPL v2",
 }
 
+struct Dispositivo {
+    numero: usize,
+}
+
+impl Dispositivo {
+    fn try_new(numero: usize) -> Result<Ref<Self>> {
+        Ref::try_new(Dispositivo { numero })
+    }
+}
+
 struct Exemplo {
     _dev: Pin<Box<miscdev::Registration<Self>>>,
 }
 
 impl FileOperations for Exemplo {
-    type OpenData = ();
+    type OpenData = Ref<Dispositivo>; // device state
+    type Wrapper = Ref<Dispositivo>; // file state
     kernel::declare_file_operations!(read, write);
 
-    fn open(_data: &(), _file: &File) -> Result {
-        pr_info!("Dispositivo sendo aberto\n");
-        Ok(())
+    fn open(data: &Ref<Dispositivo>, _file: &File) -> Result<Self::OpenData> {
+        pr_info!("Dispositivo sendo aberto: {}\n", data.numero);
+        Ok(data.clone())
     }
 
-    fn read(_this: (), _file: &File, _data: &mut impl IoBufferWriter, _offset: u64) -> Result<usize> {
+    fn read(_this: RefBorrow<'_, Dispositivo>, _file: &File, _data: &mut impl IoBufferWriter, _offset: u64) -> Result<usize> {
         
         Ok(0)
     }
 
-    fn write(_this: (),  _file: &File, data: &mut impl IoBufferReader,  _offset: u64) -> Result<usize> {
+    fn write(_this: RefBorrow<'_, Dispositivo>,  _file: &File, data: &mut impl IoBufferReader,  _offset: u64) -> Result<usize> {
         Ok(data.len())
     }
 }
@@ -41,7 +53,7 @@ impl FileOperations for Exemplo {
 impl KernelModule for Exemplo {
     fn init( _name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
         pr_info!("Olá mundo\n");
-        let reg = miscdev::Registration::new_pinned(fmt!("exemplo"), ())?;
+        let reg = miscdev::Registration::new_pinned(fmt!("exemplo"), Dispositivo::try_new(0)?)?;
         Ok(Exemplo { _dev: reg })
     }
 }
